553f6bc9d3e1acd239d1a72f2e46d48d8877231b
sonali4598 <sonaliagivale11093@gmail.com>
2022-03-01 09:31:59 +0530

    Computing 10 Basic Core Programs

---
+++ BasicCoreProgramms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicPrograms bp=new BasicPrograms();
+            //bp.Check();
+            // bp.IsLeapYear();
+            //bp.Power2();
+            //string cmdArg = Environment.GetCommandLineArgs()[1];
+            //int n = Convert.ToInt32(cmdArg);
+            //bp.Power2(n);
+            bp.isPowerOfTwo();
+            //BasicPrograms.HarmonicNo();
+            //BasicPrograms.Factors();
+            //bp.division();
+            //bp.SwapNo();
+            // bp.checkEvenOdd();
+            // bp.checkVowel();
+            // bp.LargestNo();
+            Console.ReadKey();
+        }
+    }
+}
