13d975c4ec2cb3d67b931974d25d3ddb1951ff17
sonali4598 <sonaliagivale11093@gmail.com>
2022-03-01 09:51:02 +0530

    Calculating the number is power of two or not

--- BasicCoreProgramms/BasicPrograms.cs
+++ BasicCoreProgramms/BasicPrograms.cs
@@ -58,6 +58,33 @@ namespace BasicCoreProgramms
             }
 
         }
+
+        //power of 2
+        public void isPowerOfTwo()
+        {
+            int no;
+            int remainder;
+            Console.WriteLine("******** Power Of Two *********");
+            Console.WriteLine("Enter a no to check");
+            no = Convert.ToInt32(Console.ReadLine());
+            if (no == 1)
+                Console.Write("It is power of 2.");
+            else
+            {
+                while (no > 1)
+                {
+                    remainder = no % 2;
+                    if (remainder != 0)
+                        break;
+                    no /= 2;
+                }
+                if (no == 1)
+                    Console.WriteLine("It is a power of 2");
+                else
+                    Console.WriteLine("It is not a power of 2");
+            }
+        }
+
     }
 }
 
--- BasicCoreProgramms/Program.cs
+++ BasicCoreProgramms/Program.cs
@@ -11,7 +11,7 @@ namespace BasicCoreProgramms
         static void Main(string[] args)
         {
             BasicPrograms basicPrgm=new BasicPrograms();
-            basicPrgm.IsLeapYear();   
+            basicPrgm.isPowerOfTwo();   
             Console.ReadKey();
         }
     }
